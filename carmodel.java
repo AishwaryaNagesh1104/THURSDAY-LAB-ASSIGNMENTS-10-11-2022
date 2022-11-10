c03193091be1d3315fc683d86d89eeb4b1884139
AishwaryaNagesh1104 <117056038+AishwaryaNagesh1104@users.noreply.github.com>
2022-11-10 14:58:02 +0530

    Add files via upload

---
+++ carmodel.java
@@ -0,0 +1,21 @@
+package carname;
+
+public class carmodel {
+	
+
+				public static void main(String[] args) {
+					// TODO Auto-generated method stub
+					enum car{audi(2016),tesla(2018),lambo(2010);
+					private int value;  
+					private car(int value){  
+					this.value=value;  
+					}  
+					}
+					System.out.println("    Car Name\tModel Year");
+					for(car c : car.values())
+						System.out.println("    "+c+"\t"+c.value);
+				}
+
+			}
+
+
